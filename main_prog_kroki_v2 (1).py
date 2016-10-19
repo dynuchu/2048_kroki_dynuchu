19715b0ddee7225747b9cf63dd86a724c6e3a862
dynuchu <dynuchu@gmail.com>
2016-10-19 16:37:09 +0200

    Add files via upload

---
+++ main_prog_kroki_v2 (1).py
@@ -0,0 +1,160 @@
+import random
+import os
+import sys
+
+gamebox = list()
+def init():
+    global gamebox
+    gamebox = [[0,0,0,0],
+            [0,0,0,0],      
+            [0,0,0,0],           
+            [0,0,0,0]]
+    gamebox_random()
+    gamebox_random()
+
+def gamebox_draw():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    for i in range(0,4):
+        for j in range(0,4):
+            if j == 3:
+                print(gamebox[i][j], end="\n")
+            else:
+                print(gamebox[i][j], end=" ")
+    print("\n")
+
+def gamebox_action():
+    gamebox_order()
+    gamebox_merge()
+    gamebox_order()
+
+def gamebox_validate():
+    return True
+
+def gamebox_order_left():
+    #balra visz
+    a = 0
+    while (a != 4):
+        for i in range(0,3):
+            for j in range(0,3):
+                if gamebox[a][j] == 0 and gamebox[a][j+1] !=0:
+                    gamebox[a][j] = gamebox[a][j+1]
+                    gamebox[a][j+1] = 0
+        a += 1
+
+def gamebox_order_right():
+    #balra visz
+    a = 0
+    while (a != 4):
+        for i in range(0,3):
+            for j in reversed(range(1,4)):
+                if gamebox[a][j] == 0 and gamebox[a][j-1] !=0:
+                    gamebox[a][j] = gamebox[a][j-1]
+                    gamebox[a][j-1] = 0
+        a += 1
+
+def gamebox_order_up():
+    a = 0
+    while (a !=4):
+        for j in range(0,3):
+                for i in (range(0,3)):
+                    if gamebox[i][a] == 0 and gamebox[i+1][a] !=0:
+                        gamebox[i][a] = gamebox[i+1][a]
+                        gamebox[i+1][a] = 0
+        a += 1
+
+def gamebox_order_down():
+    a = 0
+    while (a !=4):
+        for j in range(0,3):
+                for i in reversed(range(1,4)):
+                    if gamebox[i][a] == 0 and gamebox[i-1][a] !=0:
+                        gamebox[i][a] = gamebox[i-1][a]
+                        gamebox[i-1][a] = 0
+        a += 1
+
+def gamebox_merge_left():
+    a = 0
+    while (a != 4):
+        for i in range(0,3):
+            for j in range(0,3):
+                if gamebox[a][j] == gamebox[a][j+1] and gamebox[a][j] !=0:
+                    gamebox[a][j] *= 2
+                    gamebox[a][j+1] = 0
+        a += 1                
+
+def gamebox_merge_right():
+    a = 0
+    while (a != 4):
+        for i in range(0,3):
+            for j in reversed(range(1,4)):
+                if gamebox[a][j] == gamebox[a][j-1] and gamebox[a][j] !=0:
+                    gamebox[a][j] *= 2
+                    gamebox[a][j-1] = 0
+        a += 1   
+
+def gamebox_merge_up():
+    a = 0
+    while (a !=4):
+        for j in range(0,3):
+                for i in (range(0,3)):
+                    if gamebox[i][a] == gamebox[i+1][a] and gamebox[i][a]!=0: 
+                        gamebox[i][a] *= 2
+                        gamebox[i+1][a] = 0
+        a += 1
+
+
+def gamebox_merge_down():
+    a = 0
+    while (a !=4):
+        for j in range(0,3):
+                for i in reversed(range(1,4)):
+                    if gamebox[i][a] == gamebox[i-1][a] and gamebox[i][a] !=0:
+                        gamebox[i][a] *= 2
+                        gamebox[i-1][a] = 0
+        a += 1
+
+def gamebox_order_down():
+    a = 0
+    while (a !=4):
+        for j in range(0,3):
+                for i in reversed(range(1,4)):
+                    if gamebox[i][a] == 0 and gamebox[i-1][a] !=0:
+                        gamebox[i][a] = gamebox[i-1][a]
+                        gamebox[i-1][a] = 0
+        a += 1
+
+def gamebox_random():
+    while (True):
+        rnum1 = random.randrange(0,4)
+        rnum2 = random.randrange(0,4)
+        if gamebox[rnum1][rnum2] == 0:
+            gamebox[rnum1][rnum2] = 2
+            break
+
+init()
+userinput = None
+while (userinput != "quit"):
+    gamebox_draw()
+    userinput = input("Use WASD to move the blocks:")
+    if userinput == 'w':
+        gamebox_order_up()
+        gamebox_merge_up()
+        gamebox_order_up()
+        gamebox_random()
+    elif userinput == 'a':
+        gamebox_order_left()
+        gamebox_merge_left()
+        gamebox_order_left()
+        gamebox_random()
+    elif userinput == 's':
+        gamebox_order_down()
+        gamebox_merge_down()
+        gamebox_order_down()
+        gamebox_random()
+    elif userinput == 'd':
+        gamebox_order_right()
+        gamebox_merge_right()
+        gamebox_order_right()
+        gamebox_random()
+    else:
+        continue
