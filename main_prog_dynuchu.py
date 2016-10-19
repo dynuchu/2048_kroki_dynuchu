5a3fad14e13c734460d1e7da89c66aed5dbc5ef2
dynuchu <dynuchu@gmail.com>
2016-10-19 15:35:21 +0200

    Add files via upload

--- main_prog_dynuchu.py
+++ main_prog_dynuchu.py
@@ -1,10 +1,10 @@
 import random
 import os
 
-gamebox = [[0,0,4,2],
-            [0,0,8,2],      
+gamebox = [[0,0,0,0],
+            [0,2,2,0],      
             [0,8,2,0],           
-            [0,0,8,0]]
+            [2,2,0,0]]
 
 
 def gamebox_draw():
@@ -17,18 +17,97 @@ def gamebox_draw():
                 print(gamebox[i][j], end=" ")
     print("\n")
 
-def gamebox_order():
+def gamebox_order_bal():
     #balra visz
-    for i in range(0,3):
+    a = 0
+    while (a != 4):
+        for i in range(0,3):
+            for j in range(0,3):
+                if gamebox[a][j] == 0 and gamebox[a][j+1] !=0:
+                    gamebox[a][j] = gamebox[a][j+1]
+                    gamebox[a][j+1] = 0
+        a += 1
+
+def gamebox_order_jobb():
+    #balra visz
+    a = 0
+    while (a != 4):
+        for i in range(0,3):
+            for j in reversed(range(1,4)):
+                if gamebox[a][j] == 0 and gamebox[a][j-1] !=0:
+                    gamebox[a][j] = gamebox[a][j-1]
+                    gamebox[a][j-1] = 0
+        a += 1
+
+def gamebox_order_fel():
+    a = 0
+    while (a !=4):
         for j in range(0,3):
-            if gamebox[i][j] == 0 and gamebox[i][j+1] !=0:
-                gamebox[i][j] = gamebox[i][j+1]
-                gamebox[i][j+1] = 0
+                for i in (range(0,3)):
+                    if gamebox[i][a] == 0 and gamebox[i+1][a] !=0:
+                        gamebox[i][a] = gamebox[i+1][a]
+                        gamebox[i+1][a] = 0
+        a += 1
 
-    
+def gamebox_order_le():
+    a = 0
+    while (a !=4):
+        for j in range(0,3):
+                for i in reversed(range(1,4)):
+                    if gamebox[i][a] == 0 and gamebox[i-1][a] !=0:
+                        gamebox[i][a] = gamebox[i-1][a]
+                        gamebox[i-1][a] = 0
+        a += 1
 
-def gamebox_merge():
-    print("merge")
+def gamebox_merge_balra():
+    a = 0
+    while (a != 4):
+        for i in range(0,3):
+            for j in range(0,3):
+                if gamebox[a][j] == gamebox[a][j+1] and gamebox[a][j] !=0:
+                    gamebox[a][j] *= 2
+                    gamebox[a][j+1] = 0
+        a += 1                
+
+def gamebox_merge_jobbra():
+    a = 0
+    while (a != 4):
+        for i in range(0,3):
+            for j in reversed(range(1,4)):
+                if gamebox[a][j] == gamebox[a][j-1] and gamebox[a][j] !=0:
+                    gamebox[a][j] *= 2
+                    gamebox[a][j-1] = 0
+        a += 1   
+
+def gamebox_merge_fel():
+    a = 0
+    while (a !=4):
+        for j in range(0,3):
+                for i in (range(0,3)):
+                    if gamebox[i][a] == gamebox[i+1][a] and gamebox[i][a]!=0: 
+                        gamebox[i][a] *= 2
+                        gamebox[i+1][a] = 0
+        a += 1
+
+
+def gamebox_merge_le():
+    a = 0
+    while (a !=4):
+        for j in range(0,3):
+                for i in reversed(range(1,4)):
+                    if gamebox[i][a] == gamebox[i-1][a] and gamebox[i][a] !=0:
+                        gamebox[i][a] *= 2
+                        gamebox[i-1][a] = 0
+
+def gamebox_order_le():
+    a = 0
+    while (a !=4):
+        for j in range(0,3):
+                for i in reversed(range(1,4)):
+                    if gamebox[i][a] == 0 and gamebox[i-1][a] !=0:
+                        gamebox[i][a] = gamebox[i-1][a]
+                        gamebox[i-1][a] = 0
+
 def gamebox_action():
     print("action")
     gamebox_order()
@@ -48,5 +127,9 @@ def gamebox_validate():
 #~~~~~~~~~~main program~~~~~~~~~~~~
 
 gamebox_draw()
-gamebox_order()
+gamebox_order_le()
+gamebox_merge_le()
+gamebox_order_le()
 gamebox_draw()
+
+
